d189520548d717f31867bd0632a0a8185bbf9509
Spark <231098201@smail.nju.edu.cn>
2025-07-10 22:47:04 +0800

    初始SpringBoot + Vue”

---
+++ AIEDA-backend/src/main/java/org/example/aiedabackend/AiedaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.aiedabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiedaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiedaBackendApplication.class, args);
+    }
+
+}
